1c147825fb96bc962f0fbfac39783fbe47f86c7a
littlesekii <contact.sekii@gmail.com>
2026-04-08 22:26:23 -0300

    refactor: remove generic param type and simplify functions

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,46 +1,37 @@
 import { ApiError } from '@/types/error/ApiError';
 
+type HttpMethod = 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+async function request <ResultType>(url: string, method: HttpMethod, body?: unknown): Promise<ResultType> {
+  const payload: RequestInit = {
+    method: method,
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8'
+    },
+    body: body ? JSON.stringify(body) : undefined
+  };
+
+  const res = await fetch(BASE_URL + url, payload);
+
+  if (!res.ok) {
+    const errorData = await res.json().catch(() => ({}));
+    throw new ApiError(
+      errorData.statusCode,
+      errorData.errorCode,
+      errorData.message
+    );
+  }
+
+  return await res.json();
+}
+
+
 export const api = {
-
-  baseUrl: import.meta.env.VITE_API_URL,
-  request: async function <ResultType, ParamType>(url: string, method: string, body?: ParamType): Promise<ResultType> {
-    const payload: RequestInit = {
-      method: method,
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8'
-      }
-    };
-
-    if (body)
-      payload.body = JSON.stringify(body);
-
-    const res = await fetch(this.baseUrl + url, payload);
-
-    if (!res.ok) {
-      const errorData = await res.json().catch(() => ({}));
-      throw new ApiError(
-        errorData.statusCode,
-        errorData.errorCode,
-        errorData.message
-      );
-    }
-
-    const data = await res.json();
-    return data;
-  },
-  get: function <ResultType>(url: string): Promise<ResultType> {
-    return this.request(url, 'GET');
-  },
-  post: function <ResultType, ParamType>(url: string, body: ParamType): Promise<ResultType> {
-    return this.request(url, 'POST', body);
-  },
-  put: function <ResultType, ParamType>(url: string, body: ParamType): Promise<ResultType> {
-    return this.request(url, 'PUT', body);
-  },
-  patch: function <ResultType, ParamType>(url: string, body: ParamType): Promise<ResultType> {
-    return this.request(url, 'PATCH', body);
-  },
-  delete: function <ResultType, ParamType>(url: string, body: ParamType): Promise<ResultType> {
-    return this.request(url, 'DELETE', body);
-  },
+  get:    <ResultType>(url: string): Promise<ResultType> => request(url, 'GET'),
+  post:   <ResultType>(url: string, body?: unknown): Promise<ResultType> => request(url, 'POST', body),
+  put:    <ResultType>(url: string, body?: unknown): Promise<ResultType> => request(url, 'PUT', body),
+  patch:  <ResultType>(url: string, body?: unknown): Promise<ResultType> => request(url, 'PATCH', body),
+  delete: <ResultType>(url: string, body?: unknown): Promise<ResultType> => request(url, 'DELETE', body),
 };
