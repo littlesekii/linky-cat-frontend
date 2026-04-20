2f51c8f144301fed1383418aae0b2734df1bc186
littlesekii <contact.sekii@gmail.com>
2026-04-20 17:12:28 -0300

    feat: add loading icon and error handling for base button

--- src/components/common/BaseInputComponent.vue
+++ src/components/common/BaseInputComponent.vue
@@ -1,48 +1,89 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 
 const model = defineModel<string>();
 
-defineProps<{
+const props = defineProps<{
+  isLoading?: boolean,
   type?: string,
   label?: string,
+  errorMessage?: string,
   fixedLabel?: boolean
   fixedPadding?: number
 }>();
 
 const isFocused = ref(false);
 
+const inputClasses = computed(() => ({
+  'shake-animation': props.errorMessage,
+  'error': props.errorMessage,
+  'fixed-label': props.fixedLabel,
+  'default-label': !props.fixedLabel,
+  'label-moved' : !props.fixedLabel && (isFocused.value || model.value && model.value.length > 0)
+}));
+
 </script>
 
 <template>
-<div class="input">
-  <label
-    class="label"
-    :class="!fixedLabel && (isFocused || model && model.length > 0) ? 'label-moved' : ''"
-    :for="label"
-  >
-    {{ label }}
-  </label>
-  <input
-    :class="fixedLabel ? 'fixed-label-input' : 'default-label-input'"
-    :style="fixedLabel ? `padding-left: ${fixedPadding}px` : ''"
-    :id="label"
-    v-model="model"
-    :type="type || 'text'"
-    @focusin="isFocused = true"
-    @focusout="isFocused = false"
-  >
+<div class="wrapper">
+  <p class="error-message" v-if="errorMessage">{{ errorMessage }}</p>
+  <div class="input" :class="inputClasses">
+    <label
+      :for="label"
+    >
+      {{ label }}
+    </label>
+    <input
+      :style="fixedLabel ? `padding-left: ${fixedPadding}px` : ''"
+      :id="label"
+      v-model="model"
+      :type="type || 'text'"
+      @focusin="isFocused = true"
+      @focusout="isFocused = false"
+    >
+    <img v-if="isLoading" class="loading-icon" src="@/assets/loading-small.svg">
+  </div>
 </div>
 </template>
 
 <style scoped>
-.input {
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
+
+@keyframes shake {
+    0%, 100% { transform: translateX(0); }
+    20%, 60% { transform: translateX(-2px); }
+    40%, 80% { transform: translateX(2px); }
 }
 
-.label {
+.shake-animation {
+  animation: shake 0.3s ease-in-out;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+
+  gap: 2px;
+}
+
+.error-message {
+  font-size: 14px;
+  font-weight: 600;
+  color: #F53A3A;
+}
+
+.input {
+  display: flex;
+  /* flex-direction: column; */
+  gap: 5px;
+
+  background-color: #ECEEEE;
+  color: #18181A;
+
+  border: none;
+  border-radius: 10px;
+}
+
+.input label {
   font-size: 15px;
   position: absolute;
   padding: 0 13px;
@@ -56,28 +97,49 @@ const isFocused = ref(false);
   -moz-user-select: none;
 }
 
-.label-moved {
-  padding-top: 6px;
-  font-size: 12px;
-}
-
 .input input {
+  flex: 1;
   font-size: 15px;
+  outline: none;
 
-  background-color: #ECEEEE;
-  color: #18181A;
-
-  border: none;
-  border-radius: 10px;
+  border: inherit;
+  background-color: inherit;
+  border-radius: inherit;
 }
 
-.default-label-input {
+/* Altera o fundo e a cor do texto no autocomplete */
+.input input:-webkit-autofill,
+.input input:-webkit-autofill:hover,
+.input input:-webkit-autofill:focus,
+.input input:-webkit-autofill:active {
+    -webkit-box-shadow: 0 0 0 30px #ECEEEE inset !important;
+}
+
+.default-label input {
   padding: 0 13px;
   padding-top: 22px;
   padding-bottom: 6px;
 }
 
-.fixed-label-input {
+.fixed-label input {
   padding: 14px;
 }
+
+.label-moved label {
+  padding-top: 6px;
+  font-size: 12px;
+}
+
+.error {
+  box-shadow: 0 0 0 2px #F53A3A;
+}
+
+.error label {
+  color: #F53A3A;
+}
+
+.loading-icon {
+  width: 48px;
+}
+
 </style>
